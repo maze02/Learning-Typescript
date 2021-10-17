9c18a9de55e229a460b140ee1865f2065eabad7d
maze02 <71835134+maze02@users.noreply.github.com>
2021-10-17 22:58:11 +0200

    Object Literal Annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -7,3 +7,22 @@ let nothing: undefined = undefined;
 
 //built in objects
 let now: Date = new Date();
+
+//Array
+
+//Indicating an array of strings
+let colors: string[] = ['red', 'green', 'blue'];
+let nums: number[] = [10, 20, 30, 40];
+let resTruths: boolean[] = [true, false, false, true];
+
+//Classes
+
+class Car {}
+
+let car: Car = new Car(); //capitalised name refering to the type of class Car
+
+//Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
