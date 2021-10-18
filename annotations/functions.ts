38cf56151ec1f6ac43ff0482511ab47f849c7245
maze02 <71835134+maze02@users.noreply.github.com>
2021-10-18 14:41:36 +0200

    Annotations for Anonymous Functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -3,5 +3,19 @@ const add = (a: number, b: number): number => {
   return a + b;
 };
 
+const substract = (a: number, b: number): number => {
+  return a - b;
+};
 //functions takes 2 arguments
 //Typescript is concerned with types, doesn't vet the logic in the funciton
+
+//named function
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+//anonymous functions assigned to variables
+//(*can annotate the return type right after the argument list)
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
